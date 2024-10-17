d42171d3e4e3bdc1430a0160e64cbb3c7b37f55f
Tdang <tdang>
2024-10-18 00:45:00 +0200

    calculatrice complette

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ window.onload = function () {
     // do the work after everything was loaded (DOM, media elements)    
     document.querySelector('button').onclick = function () {
 
+
         var a = parseInt(document.getElementById('a').value);
         var b = parseInt(document.getElementById('b').value);
 
@@ -9,6 +10,21 @@ window.onload = function () {
             document.getElementById('result').innerHTML = 'Invalid input';
             return;
         }
-        document.getElementById('result').innerHTML = a + b;
+        switch (document.getElementById('operations').value) {
+            case 'plus':
+                document.getElementById('result').innerHTML = a + b;
+                break;
+            case 'moins':
+                document.getElementById('result').innerHTML = a - b;
+                break;
+            case 'multiplication':
+                document.getElementById('result').innerHTML = a * b;
+                break;
+            case 'division':
+                document.getElementById('result').innerHTML = a / b;
+                break;
+            default:
+                document.getElementById('result').innerHTML = 'Invalid operation';
+        }
     };
 };
